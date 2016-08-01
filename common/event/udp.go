56b76606a8a99eca4411986679029179199d65f3
yinqiwen <yinqiwen@gmail.com>
2016-08-01 09:20:56 +0800

    update

--- common/event/udp.go
+++ common/event/udp.go
@@ -11,9 +11,14 @@ type UDPEvent struct {
 }
 
 func (ev *UDPEvent) Encode(buffer *bytes.Buffer) {
+	EncodeStringValue(buffer, ev.Addr)
 	EncodeBytesValue(buffer, ev.Content)
 }
 func (ev *UDPEvent) Decode(buffer *bytes.Buffer) (err error) {
+	ev.Addr, err = DecodeStringValue(buffer)
+	if nil != err {
+		return
+	}
 	ev.Content, err = DecodeBytesValue(buffer)
 	return
 }
