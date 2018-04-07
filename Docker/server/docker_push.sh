b1208babe507646d24865507024484b52806a6e1
yinqiwen <yinqiwen@gmail.com>
2018-04-07 16:12:15 +0800

    update docker script

--- Docker/server/docker_push.sh
+++ Docker/server/docker_push.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
 docker build --build-arg GSNOVA_VER=${TRAVIS_TAG} -t gsnova/gsnova-server:${TRAVIS_TAG} ./Docker/server
 docker push gsnova/gsnova-server:${TRAVIS_TAG}
